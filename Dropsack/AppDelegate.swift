6acfad31983624ceb8468b389a022f257cfc1859
Kyle Neath <kneath@gmail.com>
2015-01-08 18:46:10 -0800

    Initial Commit

---
+++ Dropsack/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Dropsack
+//
+//  Created by Kyle Neath on 1/8/15.
+//  Copyright (c) 2015 Kyle Neath. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
